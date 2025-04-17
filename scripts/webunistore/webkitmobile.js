38ae44746631b10d79fe2d95038d00f154ca05c8
wyndchyme <161674161+wyndchyme@users.noreply.github.com>
2025-04-17 18:30:27 -0500

    Adjusted WebKit mobile fix

--- scripts/webunistore/webkitmobile.js
+++ scripts/webunistore/webkitmobile.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const isWebkitMobile = /AppleWebKit/.test(navigator.userAgent) && /Mobile|iPhone|iPad|Android/.test(navigator.userAgent);
     
     if (isWebkitMobile) {
-    document.querySelectorAll('.modIcon').forEach(icon => {
+    document.querySelectorAll('.modFooter').forEach(icon => {
         icon.classList.add('webkit-mobile-fix');
     });
     }
